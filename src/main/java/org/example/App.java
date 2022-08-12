e8d4e5df6c76d14304fe0a840288cd35c5e840e7
AndreaArbasino <andreapaolo.arbasino@mail.polimi.it>
2022-08-12 19:41:26 +0200

    DiscoverMessage multicast send/receive

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,4 @@
 package org.example;
-
-import network.DeprecatedConnection;
 import network.DiscoverSender;
 
 import java.net.UnknownHostException;
@@ -15,7 +13,7 @@ import static Utilities.StaticUtilities.DEFAULT_IP;
 public class App 
 {
     public static void main( String[] args ) throws UnknownHostException {
-        DeprecatedConnection discoverSender1 = new DeprecatedConnection(DEFAULT_IP, DEFAULT_DISCOVER_PORT);
+        DiscoverSender discoverSender1 = new DiscoverSender (DEFAULT_IP, DEFAULT_DISCOVER_PORT);
         Thread thread1 = new Thread(discoverSender1);
         thread1.start();
     }
