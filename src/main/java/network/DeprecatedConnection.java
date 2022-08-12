3a20bc6d16ecbe5f420a4de83dfcc4ecf01143da
AndreaArbasino <andreapaolo.arbasino@mail.polimi.it>
2022-08-12 18:15:56 +0200

    DiscoverMessage multicast send/receive

---
+++ src/main/java/network/DeprecatedConnection.java
@@ -0,0 +1,112 @@
+package network;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DeprecatedConnection implements Runnable{
+    private InetAddress ipAddress;
+    private int port;
+    private MulticastSocket multicastSocket;
+    private Boolean connected;
+
+    public DeprecatedConnection(String ip, int port) throws UnknownHostException {
+        this.ipAddress = InetAddress.getByName(ip);
+        this.port = port;
+        this.connected = false;
+    }
+
+    public void run(){
+        String msg = "hello";
+        try {
+            multicastSocket = new MulticastSocket(port);
+            System.out.println("port: " + multicastSocket.getPort());
+            //multicastSocket.setNetworkInterface(StaticUtilities.getLocalNetworkInterface());
+            multicastSocket.setBroadcast(true);
+            multicastSocket.joinGroup(ipAddress);
+            connected = true;
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+        System.out.println("inet address: " + multicastSocket.getInetAddress());
+        System.out.println("Is local address? " + multicastSocket.getLocalAddress().isAnyLocalAddress());
+        System.out.println("local address: " + multicastSocket.getLocalAddress());
+        System.out.println("port: " + multicastSocket.getPort());
+
+        try {
+            System.out.println("Network interface: " + multicastSocket.getNetworkInterface());
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Local socket address" + multicastSocket.getLocalSocketAddress());
+
+
+        DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(), ipAddress, port);
+        try{
+            listenDatagrams();
+            System.out.println("I am listening for messages...");
+
+            //TODO: MODIFICARE, OVVIAMENTE SCRITTO COSì PER MOTIVI DI TESTING, BISOGNA MODIFICARLO PER ADATTARSI CORRETTAMENTE A UNA FASE DI PAIRING
+            System.out.println("Type ok to send 3 messages");
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.nextLine();
+            System.out.println("You entered " + s);
+            if (s.equals("ok")) {
+                int i = 0;
+                while (i < 3){
+                    sendDatagram(hi);
+                    i++;
+                }
+            }
+        } catch (SocketException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendDatagram (DatagramPacket packet) throws IOException {
+        multicastSocket.send(packet);
+        System.out.print("I sent message of length ");
+        System.out.print(packet.getLength());
+        System.out.print(" And payload ");
+        System.out.println(Arrays.toString(packet.getData()));
+    }
+
+    public void listenDatagrams() throws IOException {
+        Thread t = new Thread(() ->{
+            while (connected){
+                try {
+                    receiveDatagram();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        t.start();
+    }
+
+    /**
+     * Receive a datagram sent to the broadcast group associated to the multicast socket
+     * @throws IOException can e raised by the "receive" method
+     */
+    public void receiveDatagram() throws IOException {
+        //TODO: VEDERE SE MANTENERE 1000 o modificarlo, togliere print di debug successivamente
+        byte[] buf = new byte[10];
+        DatagramPacket receivedPacket = new DatagramPacket(buf, buf.length);
+        multicastSocket.receive(receivedPacket);
+
+        if (receivedPacket.getAddress().equals(multicastSocket.getLocalAddress())){
+            return;
+        }
+
+        //TODO: AGGIUNGERE UN CONTROLLO PER I MESSAGGI RICEVUTI CHE SONO STATI MANDATI DA ME STESSO
+        System.out.print("I received message of length ");
+        System.out.println(receivedPacket.getLength());
+        System.out.print(" And payload ");
+        System.out.println(Arrays.toString(receivedPacket.getData()));
+    }
+}
