41e6930f80c4f55e20b4f35eb9b334689bdf51e2
AndreaArbasino <andreapaolo.arbasino@mail.polimi.it>
2022-08-19 00:20:12 +0200

    First implementation of UDP unicast communication

--- src/main/java/messages/AckMessage.java
+++ src/main/java/messages/AckMessage.java
@@ -1,10 +1,6 @@
 package messages;
 
-import java.io.Serial;
 
 public class AckMessage extends Message {
-
-    @Serial
-    private static final long serialVersionUID = -1924756960241514023L;
 
 }
