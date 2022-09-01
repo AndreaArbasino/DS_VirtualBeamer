c5546232d1ec14ca3157645c49184efd7f17a621
AlbertoPanzanini <10629004@polimi.it>
2022-09-01 16:32:28 +0200

    bully election fix (1)

--- src/main/java/timeElements/LeaderCrashTimer.java
+++ src/main/java/timeElements/LeaderCrashTimer.java
@@ -10,9 +10,7 @@ public class LeaderCrashTimer {
     private Timer timer;
     private TimerTask timerTask;
     private static final long DEFAULT_DELAY = 0;
-    private static final long MAX_ALIVE_NOT_RECEIVED = 6;
-    private static final long ALIVE_DEFUALT_LISTEN_INTERVAL = 400;
-    private static final long BASE_PERIOD =  MAX_ALIVE_NOT_RECEIVED * ALIVE_DEFUALT_LISTEN_INTERVAL;
+    private static final long BASE_PERIOD =  3000;
     private final NetworkController networkController;
     public LeaderCrashTimer(NetworkController networkController) {
         this.networkController = networkController;
@@ -26,6 +24,7 @@ public class LeaderCrashTimer {
 
     public void resetTimer(){
         timer.cancel();
+        timer.purge();
         timer = new Timer();
         timerTask = new LeaderCrashTask(networkController);
         start();
