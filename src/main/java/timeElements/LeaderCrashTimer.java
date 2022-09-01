4e9129f41d623f03ab0a407cdb9cc2997b79e81f
AndreaArbasino <andreapaolo.arbasino@mail.polimi.it>
2022-09-01 17:31:26 +0200

    ResetGroupTimer

--- src/main/java/timeElements/LeaderCrashTimer.java
+++ src/main/java/timeElements/LeaderCrashTimer.java
@@ -2,42 +2,38 @@ package timeElements;
 
 import network.NetworkController;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class LeaderCrashTimer {
 
-    private Timer timer;
-    private TimerTask timerTask;
+    private ScheduledThreadPoolExecutor threadPoolExecutor;
+    private LeaderCrashTask task;
     private static final long DEFAULT_DELAY = 0;
     private static final long BASE_PERIOD =  3000;
     private final NetworkController networkController;
+
     public LeaderCrashTimer(NetworkController networkController) {
         this.networkController = networkController;
-        timer = new Timer();
-        timerTask = new LeaderCrashTask(networkController);
+        threadPoolExecutor = new ScheduledThreadPoolExecutor(1);
+        task = new LeaderCrashTask(networkController);
     }
 
     public void start(){
-        timer.scheduleAtFixedRate(timerTask, DEFAULT_DELAY, BASE_PERIOD);
+        threadPoolExecutor.scheduleAtFixedRate(task, DEFAULT_DELAY, BASE_PERIOD, TimeUnit.MILLISECONDS);
     }
 
     public void resetTimer(){
-        timer.cancel();
-        timer.purge();
-        timerTask.cancel();
-        timer = new Timer();
-        timerTask = new LeaderCrashTask(networkController);
+        threadPoolExecutor.shutdownNow();
+        threadPoolExecutor = new ScheduledThreadPoolExecutor(1);
         start();
     }
 
     public void close(){
-        timer.cancel();
-        timerTask.cancel();
+        threadPoolExecutor.shutdownNow();
     }
 
-    private class LeaderCrashTask extends TimerTask{
-
+    private class LeaderCrashTask implements Runnable{
         private NetworkController networkController;
         public LeaderCrashTask(NetworkController controller) {
             this.networkController = controller;
